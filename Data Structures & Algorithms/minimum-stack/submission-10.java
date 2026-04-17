4100589bbdcafedc444c68059bf1598cd3f9e6bc
PRATEEK K <164144474+PRATEEKK9223@users.noreply.github.com>
2026-04-17 21:16:17 +0530

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.java
@@ -0,0 +1,37 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    //int min=Integer.MAX_VALUE;
+
+    public MinStack() {
+        stack=new Stack<>();
+        minStack=new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        }else{
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+       
+        
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty() && !minStack.isEmpty()){
+            stack.pop();
+            minStack.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
